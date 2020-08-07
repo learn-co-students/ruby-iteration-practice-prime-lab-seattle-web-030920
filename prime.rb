44229e4bf085c5922160efe47a71e90e9e7f02de
SaraKhandaker <kttsara@gmail.com>
2020-08-07 15:26:39 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime? (num)
+    return false if num < 0 or num == 0 or num == 1
+    i=2
+    while (i <= (num**0.5))
+        if num%i ==0
+            return false
+        end
+        i+=1
+    end
+    true
+end
